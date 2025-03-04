eed4ad9205ef48a26983c7f18bbf0da61200f9d6
MasterK0927 <keshav.agarwal.12325@gmail.com>
2025-03-04 10:00:00 +0530

    refactor: move c impl to new folder

--- src/file_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-
-#include "editor.h"
-
-void openFile(const char *filename, Buffer *buffer);
-void saveFile(const char *filename, Buffer *buffer);
-
-#endif // FILE_IO_H
